e88378b6e99c5a270f2d193bbeadc0ffc233f3cc
Sam Nayerman <samnayerman@gmail.com>
2017-02-12 01:41:42 -0800

    asdfasdfasdf

--- canvas.js
+++ canvas.js
@@ -7,7 +7,7 @@ $('#myCanvas2').ready(function() {
 	var world = Physics();
 	
 	var gravity = Physics.behavior('constant-acceleration', {
-		acc: {x: 0, y: 0.0004 }
+		acc: {x: 0, y: 0.001 }
 	});
 	world.add(gravity);
 
@@ -28,8 +28,63 @@ $('#myCanvas2').ready(function() {
 	//renderer.setStyle(canvas.style, ctx);
 	world.add(renderer);
 
+	//constantly called
 	world.on('step', function(){
 		world.render();
+
+		//watermark
+		ctx.font = "20px Georgia";
+		ctx.fillStyle = "rgba(0,0,0,0.5)";
+		ctx.fillText("Sam Nayerman",750, 585);
+		ctx.fillStyle = "rgba(0,0,0,1)";
+
+		//connect points for line
+		if(pointNodes==2) {
+			var newLine = new Line(tmpPoint1.x, tmpPoint2.x, tmpPoint1.y, tmpPoint2.y);
+
+			var a = newLine.x1 - newLine.x2
+			var b = newLine.y1 - newLine.y2
+
+			var c = Math.sqrt( a*a + b*b );
+			
+			var rect = Physics.body('rectangle', {
+				x: getMidPoint(tmpPoint1,tmpPoint2).x,
+				y: getMidPoint(tmpPoint1,tmpPoint2).y,
+				width: c,
+				restitution: 0.1,
+				mass: 10,
+				height: 3
+			});
+
+			var angleDeg = Math.atan2(newLine.y2 - newLine.y1, newLine.x2 - newLine.x1);
+			rect.state.angular.pos = angleDeg;
+
+			var thing = Physics.body('compound', {
+				x: getMidPoint(tmpPoint1,tmpPoint2).x,
+				y: getMidPoint(tmpPoint1,tmpPoint2).y,
+				restitution: 0.1,
+				mass: 10,
+				cof: 1,
+				height: 3,
+				children: [
+					tmpWorldPt1,
+					rect,
+					tmpWorldPt2
+				]
+			});
+
+			lines.push(rect);
+			world.add(rect);
+
+			world.remove(tmpWorldPt1);
+			world.remove(tmpWorldPt2);
+
+			tmpWorldPt1 = null;
+			tmpWorldPt2 = null;
+			tmpPoint2 = null;
+			tmpPoint1 = null;
+			pointNodes = 0;
+		}
 	});
 
 	var viewportBounds = Physics.aabb(0, 0, 900, 600);
@@ -37,13 +92,14 @@ $('#myCanvas2').ready(function() {
    // constrain objects to these bounds
    world.add(Physics.behavior('edge-collision-detection', {
       aabb: viewportBounds,
-      restitution: 0.3,
+      restitution: 0.1,
       cof: 1
    }));
 	
 	world.add( Physics.behavior('interactive', { el: renderer.el }) );
 	world.add( Physics.behavior('body-impulse-response') );
 	world.add( Physics.behavior('body-collision-detection') );
+	world.add(Physics.behavior('verlet-constraints'));
 	world.add( Physics.behavior('sweep-prune') );
 
 	Physics.util.ticker.on(function( time, dt ){
@@ -63,12 +119,6 @@ $('#myCanvas2').ready(function() {
 	var ctx = canvas.getContext("2d");
 	var cw =canvas.width;
 	var ch =canvas.height;
-
-	//watermark
-	ctx.font = "20px Georgia";
-	ctx.fillStyle = "rgba(0,0,0,0.5)";
-	ctx.fillText("Sam Nayerman",250, 250);
-	ctx.fillStyle = "rgba(0,0,0,1)";
 	
 	//currently drawn shapes on canvas
 	//position at _sleepPosMean
@@ -76,41 +126,19 @@ $('#myCanvas2').ready(function() {
 	var circles = [];
 	var triangles = [];
 	var lines = [];
+	var points = [];
 
-	var tmpPoint1;
-	var tmpPoint2;
+	var pointNodes = 0;
+
+	var tmpPoint1, tmpPoint2;
+
+	var tmpWorldPt1, tmpWorldPt2;
 	
 	var activeTool = $('#menuLeft').find('.active').attr('id');
 	
 	$('#myCanvas2').mousemove(function(e) {
 		activeTool = $('#menuLeft').find('.active').attr('id');
 
-		if(activeTool==="line" && tmpPoint1 != null && e.buttons == 1) {
-			$('#myCanvas2').on('click',function(e) {
-				tmpPoint2 = {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y};
-			});
-
-			$('#myCanvas2').off('click');
-			console.log(tmpPoint2);
-			tmpPoint2 = tmpPoint2 == null ? {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y} : tmpPoint2;
-
-			var newLine = new Line(tmpPoint1.x, tmpPoint2.x, tmpPoint1.y, tmpPoint2.y);
-
-			var a = newLine.x1 - newLine.x2
-			var b = newLine.y1 - newLine.y2
-
-			var c = Math.sqrt( a*a + b*b );
-			
-			var rect = Physics.body('rectangle', {
-				x: newLine.x1,
-				y: newLine.y1,
-				width: c,
-				height: 3
-			});
-			lines.push(rect);
-			world.add(rect);
-			tmpPoint2 = null;
-		}
 	});
 	
 	$('#myCanvas2').mousedown(function(e) {
@@ -122,6 +150,8 @@ $('#myCanvas2').ready(function() {
 			var rect = Physics.body('rectangle', {
 				x: newSquare.x,
 				y: newSquare.y,
+				mass: 10,
+				cof: 1,
 				width: newSquare.width,
 				height: newSquare.width
 			});
@@ -134,6 +164,8 @@ $('#myCanvas2').ready(function() {
 			var circ = Physics.body('circle', {
 				x: newCircle.x,
 				y: newCircle.y,
+				mass: 10,
+				cof: 1,
 				radius: newCircle.radius
 			});
 			circles.push(newCircle);
@@ -146,7 +178,9 @@ $('#myCanvas2').ready(function() {
 			var triangle = Physics.body('convex-polygon', {
 				x: newTriangle.x,
 				y: newTriangle.y,
+				mass: 10,
 				restitution: 0.1,
+				cof: 1,
 				vertices: [
 					{x: 0, y: 0},
 					{x: sideLen/2, y: -(Math.sin(60*180/Math.PI)*sideLen)},
@@ -156,10 +190,60 @@ $('#myCanvas2').ready(function() {
 			triangles.push(triangle);
 			world.add(triangle);
 		} else if(activeTool==="line") {
-			var pt1 = {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y};
-			tmpPoint1 = pt1;
-
-		}
+			if(pointNodes==0) {
+				var pt1 = {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y};
+				
+				var pt = Physics.body('circle', {
+					x: pt1.x,
+					y: pt1.y,
+					radius: 5,
+					restitution: 0,
+					mass: 10,
+					treatment: 'static',
+					styles: {
+						fillStyle: "rgba(255,0,0,1)"
+					}
+				});
+				tmpWorldPt1 = pt;
+				world.add(pt);
+				tmpPoint1 = pt1;
+				pointNodes++;
+			} else if(pointNodes==1) {
+				var pt2 = {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y};
+				
+				var pt = Physics.body('circle', {
+					x: pt2.x,
+					y: pt2.y,
+					radius: 5,
+					restitution: 0,
+					mass: 10,
+					treatment: 'static',
+					styles: {
+						fillStyle: "rgba(255,0,0,1)"
+					}
+				});
+				tmpWorldPt2 = pt;
+				world.add(pt);
+				tmpPoint2 = pt2;
+				pointNodes++;
+			}
+		} else if(activeTool==="point") {
+				var pt1 = {x: getMousePos(canvas,e).x, y: getMousePos(canvas,e).y};
+				
+				var pt = Physics.body('circle', {
+					x: pt1.x,
+					y: pt1.y,
+					radius: 5,
+					restitution: 0,
+					mass: 10,
+					treatment: 'static',
+					styles: {
+						fillStyle: "rgba(255,0,0,1)"
+					}
+				});
+				world.add(pt);
+				points.push(pt);
+			}
 	});
 
 	function getMousePos(canvas, evt) {
@@ -168,6 +252,10 @@ $('#myCanvas2').ready(function() {
 			x: evt.clientX - rect.left,
 			y: evt.clientY - rect.top
 		};
+	}
+
+	function getMidPoint(p1, p2) {
+		return {x: (p1.x+p2.x)/2, y: (p1.y+p2.y)/2};
 	}
 
 });
